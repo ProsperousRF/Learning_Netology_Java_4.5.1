5edb6360ed74ab1ab1f54baac089b8eed0a07b5b
Stanislav Rakitov <prosperousrf@gmail.com>
2021-12-04 12:36:00 +0300

    4.5.1. Пропущенные вызовы

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+  public static void main(String[] args) {
+    // write your code here
+    System.out.println("Программа: пропущенные вызовы.\n");
+
+    try (Scanner scanner = new Scanner(System.in)) {
+      while (true) {
+        printMenu();
+      }
     }
+
+  }
+
+  private static void printMenu() {
+    System.out.println("Меню:\n" +
+                               "1. Добавить контакт\n" +
+                               "2. Добавить пропущенный вызов\n" +
+                               "3. Вывести все пропущенные вызовы\n" +
+                               "4. Очистить пропущенные вызовы\n" +
+                               "5. Выход\n" +
+                               "Выберите пункт из меню (1-4):");
+  }
 }
