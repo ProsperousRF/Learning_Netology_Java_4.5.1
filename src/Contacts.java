201e715afcd81548b9733c4809f7b0449b244672
Stanislav Rakitov <prosperousrf@gmail.com>
2021-12-04 20:14:59 +0300

    4.5.1. Пропущенные вызовы. Contacts

---
+++ src/Contacts.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Stanislav Rakitov
+ */
+public class Contacts {
+  private Map<String, Contact> contacts;
+
+  public Contacts() {
+    contacts = new HashMap<>();
+  }
+
+  // Добавление контакта (имя, фамилия, номер телефона, группа контакта: работа, друзья, семья).
+  public void addContact(String name, String surname, String phone, Group group) {
+
+  }
+
+
+}
