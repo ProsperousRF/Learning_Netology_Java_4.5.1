682e176c14b93e7cec2931bb34b5d955e0cd9efb
Stanislav Rakitov <prosperousrf@gmail.com>
2021-12-04 20:08:59 +0300

    4.5.1. Пропущенные вызовы. Contact

--- src/Contact.java
+++ src/Contact.java
@@ -8,5 +8,20 @@ public class Contact {
   private String phone;
   private Group group;
 
+  public Contact(String name, String surname, String phone, Group group) {
+    this.name = name;
+    this.surname = surname;
+    this.phone = phone;
+    this.group = group;
+  }
 
+  @Override
+  public String toString() {
+    return "Contact{" +
+            "name='" + name + '\'' +
+            ", surname='" + surname + '\'' +
+            ", phone='" + phone + '\'' +
+            ", group=" + group +
+            '}';
+  }
 }
